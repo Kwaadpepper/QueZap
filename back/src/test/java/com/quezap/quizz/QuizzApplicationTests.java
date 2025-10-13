48b4b1d95c9b97be43f2a2e12f09ea1ac491a8a8
Jérémy Munsch <github@jeremydev.ovh>
2025-10-13 17:45:59 +0200

    fix: Suppression de l'annotation NonNullByDefault dans QuizzApplicationTests

--- back/src/test/java/com/quezap/quizz/QuizzApplicationTests.java
+++ back/src/test/java/com/quezap/quizz/QuizzApplicationTests.java
@@ -2,10 +2,8 @@ package com.quezap.quizz;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
 
-@NonNullByDefault
 @SpringBootTest
 class QuizzApplicationTests {
 
