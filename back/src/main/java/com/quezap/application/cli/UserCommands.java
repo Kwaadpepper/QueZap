943735afd6c6cbc84638b233d9c36c7d72ed785d
Jérémy Munsch <github@jeremydev.ovh>
2025-10-17 10:20:00 +0200

    feat: ajout de la commande pour supprimer un utilisateur avec gestion des erreurs

--- back/src/main/java/com/quezap/application/cli/UserCommands.java
+++ back/src/main/java/com/quezap/application/cli/UserCommands.java
@@ -2,6 +2,7 @@ package com.quezap.application.cli;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -11,7 +12,9 @@ import org.springframework.shell.command.annotation.Option;
 
 import com.quezap.domain.models.valueobjects.auth.RawIdentifier;
 import com.quezap.domain.models.valueobjects.auth.RawPassword;
+import com.quezap.domain.models.valueobjects.identifiers.UserId;
 import com.quezap.domain.usecases.users.AddUser;
+import com.quezap.domain.usecases.users.DeleteUser;
 import com.quezap.domain.usecases.users.ListUsers;
 import com.quezap.lib.ddd.exceptions.DomainConstraintException;
 import com.quezap.lib.ddd.exceptions.IllegalDomainStateException;
@@ -19,15 +22,21 @@ import com.quezap.lib.pagination.PageRequest;
 
 import org.jline.reader.LineReader;
 
+@Command(command = "users", description = "User management commands")
 public class UserCommands {
   private final ListUsers.Handler listUsersHandler;
   private final AddUser.Handler addUserHandler;
+  private final DeleteUser.Handler deleteUserHandler;
 
   @Autowired @Lazy private LineReader lineReader;
 
-  public UserCommands(ListUsers.Handler listUsersHandler, AddUser.Handler addUserHandler) {
+  public UserCommands(
+      ListUsers.Handler listUsersHandler,
+      AddUser.Handler addUserHandler,
+      DeleteUser.Handler deleteUserHandler) {
     this.listUsersHandler = listUsersHandler;
     this.addUserHandler = addUserHandler;
+    this.deleteUserHandler = deleteUserHandler;
   }
 
   @Command(command = "list", description = "list users")
@@ -79,7 +88,39 @@ public class UserCommands {
     }
   }
 
+  @Command(
+      command = {"delete"},
+      description = "Delete a user")
+  String deleteUser(
+      @Option(required = true, arity = CommandRegistration.OptionArity.EXACTLY_ONE)
+          String loginOrId) {
+    try {
+      if (isUserId(loginOrId)) {
+        final var input = new DeleteUser.Input.Id(new UserId(UUID.fromString(loginOrId)));
+        deleteUserHandler.handle(input);
+      } else {
+        final var input = new DeleteUser.Input.UserName(loginOrId);
+        deleteUserHandler.handle(input);
+      }
+
+      return "Done";
+    } catch (IllegalDomainStateException e) {
+      return "Error : " + e.getMessage();
+    } catch (DomainConstraintException e) {
+      return "Error " + e.getMessage();
+    }
+  }
+
   private String readPassword(String prompt) {
     return lineReader.readLine(prompt, null);
+  }
+
+  private boolean isUserId(String str) {
+    try {
+      new UserId(UUID.fromString(str));
+      return true;
+    } catch (IllegalDomainStateException | IllegalArgumentException _) {
+      return false;
+    }
   }
 }
