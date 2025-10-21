3b21464ced82ff37cc74c16c0c5e07559bef825f
Jérémy Munsch <github@jeremydev.ovh>
2025-10-21 15:14:27 +0200

    feat: ajouter le dépôt en mémoire pour la gestion des questions

---
+++ back/src/main/java/com/quezap/infrastructure/adapter/repositories/QuestionInMemoryRepository.java
@@ -0,0 +1,36 @@
+package com.quezap.infrastructure.adapter.repositories;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Repository;
+
+import com.quezap.domain.models.entities.Question;
+import com.quezap.domain.port.repositories.QuestionRepository;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+@Repository
+public class QuestionInMemoryRepository implements QuestionRepository {
+  private final ConcurrentHashMap<UUID, Question> storage = new ConcurrentHashMap<>();
+
+  @Override
+  public @Nullable Question find(UUID id) {
+    return storage.get(id);
+  }
+
+  @Override
+  public void save(Question entity) {
+    storage.put(entity.getId(), entity);
+  }
+
+  @Override
+  public void update(Question entity) {
+    storage.put(entity.getId(), entity);
+  }
+
+  @Override
+  public void delete(Question entity) {
+    storage.remove(entity.getId());
+  }
+}
