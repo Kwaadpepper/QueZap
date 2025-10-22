8464e01ab0a21ff7bb98e6fc17ce97f878770c56
Jérémy Munsch <github@jeremydev.ovh>
2025-10-22 18:42:15 +0200

    test: ajouter la configuration de test pour l'application Quizz

--- back/src/test/java/com/quezap/quizz/QuizzApplicationTests.java
+++ back/src/test/java/com/quezap/quizz/QuizzApplicationTests.java
@@ -1,12 +1,16 @@
 package com.quezap.quizz;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuizzApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    // Test de démarrage du contexte Spring - pas d'implémentation nécessaire
+  }
 }
