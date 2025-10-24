d62532a16a278be193b040c1c9f6a647ee32e859
Jérémy Munsch <github@jeremydev.ovh>
2025-10-24 12:04:21 +0200

    feat: ajouter la configuration de sécurité avec gestion des sessions sans état

--- back/src/main/java/com/quezap/application/config/SpringSecurityConfig.java
+++ back/src/main/java/com/quezap/application/config/SpringSecurityConfig.java
@@ -2,8 +2,14 @@ package com.quezap.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -12,5 +18,32 @@ public class SpringSecurityConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
     return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+  }
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    return http.sessionManagement(
+            // No cookie session, just state less API.
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // No CSRF for stateless APIs.
+        .csrf(AbstractHttpConfigurer::disable)
+        .exceptionHandling(
+            handling ->
+                handling.authenticationEntryPoint(
+                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .authorizeHttpRequests(
+            request -> {
+
+              // Allow all for now
+              request.anyRequest().permitAll();
+
+              // Allow root path.
+              // request.requestMatchers("/").permitAll();
+
+              // Any other routes are.
+              // request.anyRequest().fullyAuthenticated();
+            })
+        .build();
   }
 }
