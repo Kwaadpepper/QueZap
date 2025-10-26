92068e42f788fb87a668043ada07bb44b7ab1184
Jérémy Munsch <github@jeremydev.ovh>
2025-10-26 14:25:42 +0100

    refactor: simplifier la gestion de QuestionPictureManager dans QuestionDi

--- back/src/main/java/com/quezap/application/dependencies/QuestionDi.java
+++ back/src/main/java/com/quezap/application/dependencies/QuestionDi.java
@@ -1,11 +1,8 @@
 package com.quezap.application.dependencies;
 
-import java.util.stream.Stream;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.quezap.domain.models.valueobjects.pictures.Picture;
 import com.quezap.domain.port.repositories.QuestionRepository;
 import com.quezap.domain.port.repositories.ThemeRepository;
 import com.quezap.domain.port.services.QuestionPictureManager;
@@ -16,10 +13,15 @@ import com.quezap.domain.usecases.questions.ListQuestions;
 public class QuestionDi {
   private final QuestionRepository questionRepository;
   private final ThemeRepository themeRepository;
+  private final QuestionPictureManager questionPictureManager;
 
-  public QuestionDi(QuestionRepository questionRepository, ThemeRepository themeRepository) {
+  public QuestionDi(
+      QuestionRepository questionRepository,
+      ThemeRepository themeRepository,
+      QuestionPictureManager questionPictureManager) {
     this.questionRepository = questionRepository;
     this.themeRepository = themeRepository;
+    this.questionPictureManager = questionPictureManager;
   }
 
   @Bean
@@ -29,36 +31,6 @@ public class QuestionDi {
 
   @Bean
   AddQuestion.Handler addQuestionHandler() {
-    return new AddQuestion.Handler(questionRepository, themeRepository, questionPictureManager());
-  }
-
-  @Bean
-  QuestionPictureManager questionPictureManager() {
-    return new QuestionPictureManager() {
-
-      @Override
-      public Picture store(Stream<Byte> pictureData) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'store'");
-      }
-
-      @Override
-      public boolean exists(Picture picture) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'exists'");
-      }
-
-      @Override
-      public Picture copy(Picture picture) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'copy'");
-      }
-
-      @Override
-      public void remove(Picture picture) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
-      }
-    };
+    return new AddQuestion.Handler(questionRepository, themeRepository, questionPictureManager);
   }
 }
