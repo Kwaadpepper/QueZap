81665f330df01fdfac4f59ffebe157732c9f43e1
Jérémy Munsch <github@jeremydev.ovh>
2025-10-27 14:29:00 +0100

    feat(themes): ajouter le contrôleur FindTheme et DTOs associés
    * Création de FindThemesDto pour la recherche de thèmes.
    * Ajout de ThemeDto pour la réponse des thèmes.
    * Mise en place du contrôleur FindThemeController pour gérer les requêtes de recherche de thèmes avec pagination.
    * Gestion des exceptions de pagination intégrée.

---
+++ back/src/main/java/com/quezap/application/api/v1/dto/response/themes/ThemeDto.java
@@ -0,0 +1,5 @@
+package com.quezap.application.api.v1.dto.response.themes;
+
+import java.util.UUID;
+
+public record ThemeDto(UUID uuid, String name) {}
---
+++ back/src/main/java/com/quezap/application/api/v1/routes/themes/FindThemeController.java
@@ -0,0 +1,67 @@
+package com.quezap.application.api.v1.routes.themes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quezap.application.api.v1.dto.request.PaginationDto;
+import com.quezap.application.api.v1.dto.request.themes.FindThemesDto;
+import com.quezap.application.api.v1.dto.response.PageOfDto;
+import com.quezap.application.api.v1.dto.response.themes.ThemeDto;
+import com.quezap.application.api.v1.exceptions.BadPaginationException;
+import com.quezap.domain.usecases.themes.ListThemes;
+import com.quezap.lib.ddd.exceptions.IllegalDomainStateException;
+import com.quezap.lib.pagination.Pagination;
+
+import jakarta.validation.Valid;
+
+@RestController("themes")
+public class FindThemeController {
+  private final ListThemes.Handler handler;
+
+  FindThemeController(ListThemes.Handler handler) {
+    this.handler = handler;
+  }
+
+  @GetMapping("apiv1/themes/find")
+  PageOfDto<ThemeDto> find(@Valid PaginationDto paginationDto, @Valid FindThemesDto queryDto) {
+    final var input = toInput(paginationDto, queryDto);
+    final var output = handler.handle(input);
+
+    return PageOfDto.fromDomain(output.value(), this::toDto);
+  }
+
+  private ListThemes.Input toInput(PaginationDto paginationDto, FindThemesDto queryDto) {
+    final var pagination = toDomain(paginationDto);
+    final var search = queryDto.search();
+
+    if (search != null) {
+      return new ListThemes.Input.Searching(pagination, search);
+    }
+
+    return new ListThemes.Input.PerPage(pagination);
+  }
+
+  private Pagination toDomain(PaginationDto dto) {
+    try {
+      final var page = dto.page();
+      final var perPage = dto.perPage();
+      final var from = dto.from();
+      final var to = dto.to();
+
+      if (page != null && perPage != null) {
+        return Pagination.ofPage(page, perPage);
+      } else if (from != null && to != null) {
+        return Pagination.ofIndexes(from, to);
+      }
+      return Pagination.firstPage();
+    } catch (IllegalDomainStateException e) {
+      final var errorMessage = e.getMessage();
+
+      throw new BadPaginationException(errorMessage);
+    }
+  }
+
+  private ThemeDto toDto(ListThemes.Output.ThemeDto theme) {
+    return new ThemeDto(theme.id().value(), theme.name().value());
+  }
+}
