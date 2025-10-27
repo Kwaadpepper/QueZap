b5620a0c5beb03c3b5e6ab19b00171a3035a20fb
Jérémy Munsch <github@jeremydev.ovh>
2025-10-27 16:42:44 +0100

    fix(test): corriger l'importation de ThemeId dans RenameThemeTest
    - Ajout de l'importation de ThemeId pour résoudre les erreurs de compilation

--- back/src/test/java/com/quezap/domain/usecases/themes/RenameThemeTest.java
+++ back/src/test/java/com/quezap/domain/usecases/themes/RenameThemeTest.java
@@ -2,6 +2,7 @@ package com.quezap.domain.usecases.themes;
 
 import com.quezap.domain.models.entities.Theme;
 import com.quezap.domain.models.valueobjects.ThemeName;
+import com.quezap.domain.models.valueobjects.identifiers.ThemeId;
 import com.quezap.domain.port.repositories.ThemeRepository;
 import com.quezap.lib.ddd.exceptions.DomainConstraintException;
 
