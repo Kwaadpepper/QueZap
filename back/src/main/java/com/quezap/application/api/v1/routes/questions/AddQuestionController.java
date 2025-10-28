7eeeb3f3b5ac725ae7de82631e5a90c2622debb5
Jérémy Munsch <github@jeremydev.ovh>
2025-10-28 13:15:19 +0100

    feat(api): ajouter le contrôleur AddQuestion et le DTO NewAffirmation
    - Créé le DTO NewAffirmationDto pour encapsuler les données d'affirmation
    - Implémenté le contrôleur AddQuestionController pour gérer les requêtes d'ajout d'affirmation
    - Ajouté la validation du fichier image avec PictureMetadataValidatorService
    - Géré les exceptions avec ServerException lors de l'échec de la lecture du fichier

---
+++ back/src/main/java/com/quezap/application/api/v1/routes/questions/AddQuestionController.java
@@ -0,0 +1,58 @@
+package com.quezap.application.api.v1.routes.questions;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quezap.application.api.v1.dto.request.questions.NewAffirmationDto;
+import com.quezap.application.api.v1.exceptions.ServerException;
+import com.quezap.application.services.PictureMetadataValidatorService;
+import com.quezap.domain.models.valueobjects.pictures.PictureType;
+import com.quezap.domain.models.valueobjects.pictures.PictureUploadData;
+import com.quezap.domain.usecases.questions.AddQuestion;
+
+@RestController
+public class AddQuestionController {
+  private final AddQuestion.Handler handler;
+  private final PictureMetadataValidatorService validatorService; // Injecté
+
+  AddQuestionController(
+      AddQuestion.Handler handler, PictureMetadataValidatorService validatorService) {
+    this.handler = handler;
+    this.validatorService = validatorService;
+  }
+
+  @PostMapping("apiv1/questions/affirmation")
+  public String addAffirmation(@RequestBody NewAffirmationDto request) {
+    final var question = request.question();
+    final var isTrue = request.isTrue();
+    final var picture = request.picture();
+    final var themeId = request.themeId();
+
+    try (final var pictureStream = picture.getInputStream()) {
+
+      final String mimeType = validatorService.validateAndGetMimeType(picture);
+      final PictureUploadData pictureData =
+          buildPictureUploadData(pictureStream, mimeType, picture.getSize());
+
+      final var input = new AddQuestion.Input.Affirmation(question, isTrue, pictureData, themeId);
+      final var output = handler.handle(input);
+
+      return output.id().toString();
+
+    } catch (IOException e) {
+      throw new ServerException("Failed to read uploaded file", e);
+    }
+  }
+
+  private PictureUploadData buildPictureUploadData(
+      InputStream fileStream, String mimeType, long fileSize) {
+
+    final var pictureType = PictureType.fromMimeType(mimeType);
+
+    return new PictureUploadData(fileStream, fileSize, pictureType);
+  }
+}
