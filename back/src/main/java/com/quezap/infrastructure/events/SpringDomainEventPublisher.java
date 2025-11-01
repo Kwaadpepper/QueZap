183e16bae0c9b1c72d0359f36a3bed38b1f3ba33
Jérémy Munsch <github@jeremydev.ovh>
2025-11-01 12:09:02 +0100

    refactor(events): deplacement de SpringDomainEventPublisher vers infrastructure et ajout du role infrastructure

--- back/src/main/java/com/quezap/infrastructure/events/SpringDomainEventPublisher.java
+++ back/src/main/java/com/quezap/infrastructure/events/SpringDomainEventPublisher.java
@@ -1,12 +1,15 @@
-package com.quezap.application.event;
+package com.quezap.infrastructure.events;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
 
 import com.quezap.lib.ddd.events.DomainEvent;
 import com.quezap.lib.ddd.events.DomainEventPublisher;
 
 @Component
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class SpringDomainEventPublisher implements DomainEventPublisher {
   private final ApplicationEventPublisher applicationEventPublisher;
 
