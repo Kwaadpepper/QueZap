3b3ff19d709b4de3ca46573d557315a931b12b86
Jérémy Munsch <github@jeremydev.ovh>
2025-11-07 21:39:52 +0100

    feat(front): add paginator component with functionality
    Implement a paginator component to manage pagination in the application. This includes
    HTML template, TypeScript logic, and corresponding tests to ensure proper functionality.

---
+++ front/src/app/shared/components/paginator/paginator.ts
@@ -0,0 +1,46 @@
+import { JsonPipe } from '@angular/common'
+import { Component, computed, input, output } from '@angular/core'
+
+import { PaginatorModule, PaginatorState } from 'primeng/paginator'
+
+export interface PaginatorPageInfo {
+  page: number
+  pageSize: number
+  totalElements: number
+  totalPages: number
+}
+
+export interface PaginatorEvent {
+  page: number
+  pageSize: number
+}
+
+@Component({
+  selector: 'quizz-paginator',
+  imports: [PaginatorModule, JsonPipe],
+  templateUrl: './paginator.html',
+})
+export class Paginator {
+  protected readonly defaultPerPage = 10
+  protected readonly perPageOptions = [2, 5, this.defaultPerPage, 25, 35, 50]
+
+  public readonly pageInfo = input.required<PaginatorPageInfo>()
+  public readonly pageSize = input<number>(this.defaultPerPage)
+
+  public readonly paginationChanged = output<PaginatorEvent>()
+
+  // Adaptative display properties
+  protected readonly showFirstLastIcon = computed(() => this.pageInfo().totalPages > 5)
+  protected readonly showJumpToPageDropdown = computed(() => this.pageInfo().totalPages > 10)
+  protected readonly showPageLinks = computed(() => this.pageInfo().totalPages <= 20)
+  protected readonly showCurrentPageReport = computed(() => true)
+
+  protected onPageChange(event: PaginatorState): void {
+    const page = (event.page ?? 0) + 1
+    const pageSize = event.rows ?? this.defaultPerPage
+
+    console.debug('Page changed:', { page, pageSize })
+
+    this.paginationChanged.emit({ page, pageSize })
+  }
+}
