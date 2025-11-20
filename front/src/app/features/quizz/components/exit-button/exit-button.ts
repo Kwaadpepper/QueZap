6bd5aa33b9c1650ab2774cd5813a4912fd7d3d26
Jérémy Munsch <github@jeremydev.ovh>
2025-11-20 17:50:18 +0100

    feat(front): add confirmation popup for exiting quiz
    Implement a confirmation dialog to prevent accidental exits from the quiz.
    This enhances user experience by ensuring users are aware of their actions.

--- front/src/app/features/quizz/components/exit-button/exit-button.ts
+++ front/src/app/features/quizz/components/exit-button/exit-button.ts
@@ -1,19 +1,31 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { Router } from '@angular/router'
 
+import { ConfirmationService } from 'primeng/api'
 import { Button } from 'primeng/button'
+import { ConfirmPopupModule } from 'primeng/confirmpopup'
 
 @Component({
   selector: 'quizz-exit-button',
-  imports: [Button],
+  imports: [
+    Button,
+    ConfirmPopupModule,
+  ],
+  providers: [ConfirmationService],
   templateUrl: './exit-button.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExitButton {
   readonly #exitUrl = '/'
   private readonly router = inject(Router)
+  private readonly confirmationService = inject(ConfirmationService)
 
-  protected onQuitQuizz() {
-    this.router.navigateByUrl(this.#exitUrl)
+  protected onQuitQuizz($event: Event): void {
+    this.confirmationService.confirm({
+      target: $event.currentTarget as EventTarget,
+      accept: () => {
+        this.router.navigateByUrl(this.#exitUrl)
+      },
+    })
   }
 }
