fe706d27665c35065fc0fc66457d08beed076083
Jérémy Munsch <github@jeremydev.ovh>
2025-11-26 18:01:05 +0100

    refactor(front): enhance answer computation logic
    Refactor the answer computation to handle different question types
    more effectively, ensuring only relevant answers are returned
    for Boolean and Binary types.

--- front/src/app/features/admin/quezaps/components/quezap-editor/parts/question-editor/parts/answer-editor/answer-editor.ts
+++ front/src/app/features/admin/quezaps/components/quezap-editor/parts/question-editor/parts/answer-editor/answer-editor.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core'
 
+import { QuestionType } from '@quezap/domain/models'
+
 import { QuezapEditorContainer } from '../../../../editor-container'
 
 import { AnswerComponent } from './parts'
@@ -13,5 +15,16 @@ import { AnswerComponent } from './parts'
 export class AnswerEditor {
   private readonly editorContainer = inject(QuezapEditorContainer)
 
-  protected readonly answers = computed(() => this.editorContainer.selectedQuestion().answers)
+  protected readonly selectedQuestion = computed(() => this.editorContainer.selectedQuestion())
+  protected readonly answers = computed(() => {
+    switch (this.selectedQuestion().type) {
+      case QuestionType.Boolean:
+        return this.selectedQuestion().answers.slice(0, 2)
+      case QuestionType.Binary:
+        return this.selectedQuestion().answers.slice(0, 2)
+      case QuestionType.Quizz:
+      default:
+        return this.selectedQuestion().answers
+    }
+  })
 }
